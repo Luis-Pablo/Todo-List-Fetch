a58b3b5fae90fef3652a66fd0018e8e8ff56780f
Luis Pablo <pabloarias024@gmail.com>
2023-03-12 17:24:27 -0300

    todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
 
 function App() {
+  const [input, setInput] = useState("");
+  const [tarea, setTarea] = useState([]);
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (input !== "") {
+      setTarea(tarea.concat(input));
+      setInput('')
+    }
+  }
+
+
+
+  console.log(tarea)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container  justify-content-center my-5">
+
+      <h2 className="d-block fw-bold ">Lista de tareas</h2>
+
+      <form onSubmit={handleSubmit} className="form-control">
+        <input type="text"
+          className="form-control"
+          placeholder="¿Qué quieres hacer hoy?"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+
+          onChange={handleChange}
+          value={input}
+        />
+
+      </form >
+
+      <span className="form-text my-2" id="span">Tareas pendientes : {tarea.length}</span>
+
+
+
+      <div className="mt-3">
+        <ul>
+          {tarea.map((item, index) =>
+            <div className="row input-group mb-3">
+              <li className="form-control col-md-6" key={index}>{item} </li>
+
+              <button onClick={() => setTarea(tarea.filter(borrar => borrar != item))}
+                className="justify-content-around btn btn-danger col-md-2 "
+                type="button">
+
+                Borrar
+              </button>
+            </div>
+
+          )}
+
+
+        </ul>
+
+      </div>
+
+
+
+
+
+
+
     </div>
-  );
+  )
 }
 
 export default App;
